cb825c592062d703ff2186189a241cd9d5d7d19f
L0CKCaps <capslocksoftware@hotmail.com>
2021-12-11 00:02:59 +0300

    Proje dosyası ekle.

---
+++ asallar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace asallar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            for (int i = 2; i < 20; i++)
+            {
+               bool kontrol = false;
+                for (int x = 2; x < i; x++)
+                {
+                    if (i % x == 0)
+                    {                        
+                        kontrol = true;
+                        break;
+                    }
+                }
+                if (kontrol == false)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
